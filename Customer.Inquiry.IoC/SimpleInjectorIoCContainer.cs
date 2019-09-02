68f39632e25d4d8bb5ccbee0a74d90c1abe95201
Sarayuth Manmungsin <sarayuth_jack_manmungsin@hotmail.com>
2019-09-02 16:43:38 +0700

    Prepare IoC base.

---
+++ Customer.Inquiry.IoC/SimpleInjectorIoCContainer.cs
@@ -0,0 +1,26 @@
+﻿using SimpleInjector;
+using SimpleInjector.Integration.Web.Mvc;
+using System.Web.Mvc;
+
+namespace Customer.Inquiry.IoC
+{
+    public class SimpleInjectorIoCContainer
+    {
+        private static Container _container;
+
+        public static void Prepare()
+        {
+            _container = new Container();
+
+            //_container.Register<IUserService, UserService>(Lifestyle.Transient);
+            //_container.Register<IUserRepository, SqlUserRepository>(Lifestyle.Singleton);
+
+            _container.Verify();
+
+            // store the container for use by the application
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(_container));
+        }
+
+
+    }
+}
